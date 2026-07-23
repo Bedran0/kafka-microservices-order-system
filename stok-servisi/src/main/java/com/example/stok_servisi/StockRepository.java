a2f3df4970c267cbd04c1c6b5cdf5f709415485e
Bedran Büyükyorulmaz <113429938+Bedran0@users.noreply.github.com>
2026-07-23 13:43:47 +0300

    Update StockRepository.java

--- stok-servisi/src/main/java/com/example/stok_servisi/StockRepository.java
+++ stok-servisi/src/main/java/com/example/stok_servisi/StockRepository.java
@@ -4,7 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
-    // Urun adina gore stok bulmak icin (Spring bu metodu otomatik yazar)
     Optional<Stock> findByProduct(String product);
 }
