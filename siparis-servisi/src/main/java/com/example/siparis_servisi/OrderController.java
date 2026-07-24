2fcff311357a706fda69b2849f469a03d47df999
Bedran Büyükyorulmaz <113429938+Bedran0@users.noreply.github.com>
2026-07-24 21:04:55 +0300

    Update OrderController.java

--- siparis-servisi/src/main/java/com/example/siparis_servisi/OrderController.java
+++ siparis-servisi/src/main/java/com/example/siparis_servisi/OrderController.java
@@ -27,29 +27,24 @@ public class OrderController {
 
     @PostMapping
     public Order createOrder(@RequestBody OrderRequest request) {
-        // 1. Musteriyi bul, yoksa olustur
         Customer customer = customerRepository.findByName(request.getCustomerName())
                 .orElseGet(() -> new Customer(request.getCustomerName()));
 
-        // 2. Siparisi olustur ve musteriye bagla
         Order order = new Order();
         order.setOrderId(UUID.randomUUID().toString());
         order.setStatus("BEKLEMEDE");
         order.setReason("Siparis alindi, stok kontrol ediliyor");
         order.setCustomer(customer);
 
-        // 3. Her item icin bir OrderItem olustur ve siparise bagla
         for (OrderRequest.ItemRequest itemReq : request.getItems()) {
             OrderItem item = new OrderItem(itemReq.getProduct(), itemReq.getQuantity());
             item.setOrder(order);
             order.getItems().add(item);
         }
 
-        // 4. Kaydet (cascade ile musteri + siparis + itemlar birlikte)
         customer.getOrders().add(order);
         customerRepository.save(customer);
 
-        // 5. Kafka'ya gonderilecek mesaji hazirla
         OrderMessage message = new OrderMessage();
         message.setOrderId(order.getOrderId());
         message.setCustomerName(customer.getName());
@@ -57,7 +52,6 @@ public class OrderController {
             message.getItems().add(new OrderMessage.ItemMessage(item.getProduct(), item.getQuantity()));
         }
 
-        // 6. Kafka'ya yolla
         orderProducer.sendOrder(message);
 
         return order;
